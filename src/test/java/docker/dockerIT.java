c5e52227e2a41e8eb9c7d2091c890db3d68cb9ab
Cesar Tobon <cesar.tobon@globalbank.com.pa>
2024-02-08 16:40:26 -0500

    se adiciona project

---
+++ src/test/java/docker/dockerIT.java
@@ -0,0 +1,8 @@
+package docker;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class dockerIT extends dockerTest {
+    // Execute the same tests but in packaged mode.
+}
